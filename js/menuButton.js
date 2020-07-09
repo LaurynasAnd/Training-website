6d50f31ce97fff30d8bd45e3b007e8537a9f1292
LaurynasAnd <67257508+LaurynasAnd@users.noreply.github.com>
2020-07-09 15:24:10 +0300

    404 header #28

---
+++ js/menuButton.js
@@ -0,0 +1,12 @@
+function clickMenuButton(){
+    console.log("working");
+    
+    // let button = document.getElementsByClassName('menu-btn')
+    
+    // button.onclick = function () {
+    //     button.classList.toggle('change');
+    //     return
+    // }
+}
+
+export default clickMenuButton;
