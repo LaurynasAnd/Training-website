75d1b4cd05f69363cda99cfbfb91433a438facc6
LaurynasAnd <67257508+LaurynasAnd@users.noreply.github.com>
2020-07-11 18:13:03 +0300

    Menu tab in header working properly

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 import clickMenuButton from './menuButton.js';
-import mouseOutMenu from './mouseOutMenu.js'
+
 
 const menu = document.querySelector('.menu-btn')
 menu.addEventListener('click', clickMenuButton);
 
-const menu2 = document.querySelector('.nav-tab')
-menu2.addEventListener('mouseout', mouseOutMenu);
+
 // clickMenuButton();
--- js/menuButton.js
+++ js/menuButton.js
@@ -1,25 +1,12 @@
+import mouseLeaveMenu from './mouseLeaveMenu.js';
 function clickMenuButton(){
-    // console.log("working");
-    console.log('on click');
-    
-    let button = document.querySelector('.menu-btn');
     let nav = document.querySelector('.nav-tab')
     let link = document.querySelectorAll('.link');
-    // console.log(button);
-    // console.log(link);
-    // button.onclick = function () {
-        button.classList.toggle('change');
         nav.classList.toggle('visible-nav');
-        // nav.classList.toggle('nav-tab');
-        for (let i = 0; i < link.length; i++){
-            // link[i].classList.toggle('visible-link');
-            if (i === link.length - 1){
-            link[i].classList.toggle('button');
-            }
-        }
-    // }
+            link[link.length-1].classList.toggle('button');
     
-
+            const mouseLeft = document.querySelector('.nav-tab')
+            mouseLeft.addEventListener('mouseleave', mouseLeaveMenu);
     return
 }
 
